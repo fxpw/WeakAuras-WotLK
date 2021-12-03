e8c82d8f1f07e26fbedac816113b3e1de5f8a5e6
fxpw <fxpw@outlook.com>
2021-12-03 15:01:17 +0300

    fix

--- WeakAuras/Prototypes.lua
+++ WeakAuras/Prototypes.lua
@@ -3765,7 +3765,6 @@ Private.event_prototypes = {
     events = {
       ["events"] = {
         "SPELL_UPDATE_USABLE",
-        "ACTIONBAR_UPDATE_USABLE",
         "PLAYER_TARGET_CHANGED",
         "RUNE_POWER_UPDATE",
         "RUNE_TYPE_UPDATE",
