32b3c66ab7cf98b6204cdc20f2e6c4e44b4512fc
fxpw <fxpw@outlook.com>
2023-04-03 19:07:13 +0300

    fix

--- WeakAuras/Init.lua
+++ WeakAuras/Init.lua
@@ -8,7 +8,7 @@ WeakAuras.halfWidth = WeakAuras.normalWidth / 2
 WeakAuras.doubleWidth = WeakAuras.normalWidth * 2
 
 local versionStringFromToc = GetAddOnMetadata("WeakAuras", "Version")
-local versionString = "4.0.91"
+local versionString = "4.0.92"
 local buildTime = "20201210233053"
 
 WeakAuras.versionString = versionStringFromToc
